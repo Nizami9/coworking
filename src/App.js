44d4c326c15255cab25f6b380b96a7f7bafe7efe
Nizami Suleymanov <nizami.suleymanoff@gmail.com>
2022-12-13 10:35:31 +0100

    init with github

---
+++ src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div>
+    <h1>Lets do it, my dream team!</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import {BrowseRouter, Router, Route, Switch, Redirect} from 'react-dom';
+import express from 'express';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
