a9fa58afa4e6b023be3ed5ae2610a6b3b9aeefb6
Nizami Suleymanov <nizami.suleymanoff@gmail.com>
2022-12-21 14:57:21 +0100

    adding cascade of footer

---
+++ src/Components/Footer.js/Footer.js
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='containerFooter'>
+        <div className='leftContainer'>
+            <img className='footerLogo' />
+            <p>It starts with good people</p>
+            <p>Become a part of our community</p>
+            <div className='socialPagesFooter'>
+                <a><img /></a>
+                <a><img /></a>
+                <a><img /></a>
+                <a><img /></a>
+            </div>
+            <p>Copyright © 2022 Companies  All rights reserved</p>
+        </div>
+        <div className='rightContainer'>
+            <h3>Contact Us</h3>
+            <form>
+                
+            </form>
+        </div>
+    </div>
+  )
+}
+
+export default Footer
