ca91d1626bf283b9e1babd9aa62c1ff3fe6e20e2
Shafiaks <113788543+Shafiaks@users.noreply.github.com>
2022-12-22 15:10:53 +0100

    added login page

---
+++ src/Components/MainPage/Locations.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Locations = () => {
+  return (
+    <div  style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh'
+    }} >
+      <h1>locations</h1>
+    </div>
+  )
+}
+
+export default Locations
