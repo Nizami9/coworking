f4bfa968da7521fb1e7347acafe998847242aad1
Nizami Suleymanov <nizami.suleymanoff@gmail.com>
2022-12-23 02:50:36 +0100

    not yet finish the main page

--- src/Components/MainPage/Home/Home.js
+++ src/Components/MainPage/Home/Home.js
@@ -1,6 +1,6 @@
 import "./Home.css"
 import "./MonumentExtended-FreeForPersonal/MonumentExtended-Regular.otf";
-
+import { NavLink } from "react-router-dom";
 const Home = () => {
   return ( 
     <div className="container">
@@ -26,6 +26,61 @@ const Home = () => {
         </div>
       </div>
       <div className="rightContainerMain"></div>
+      <div className="bottomContainer">
+        <div className="bottomContainerLeft">
+        <h1>Book a unique space for your activity</h1>
+        <p>From a single desk to a whole building <br/>The choice is yours</p>
+        </div>
+        <div className="bottomContainerRight">
+          <div className="citesOptionPictures">
+          <NavLink to="/locations" className='pic1'
+          style={({ isActive }) => ({
+            color: isActive ? '#ffffff' : '#ffffff',
+            textDecoration: isActive ? 'none' : 'none',
+          })}>
+              <p>Hamburg</p>
+          </NavLink>
+          <div className="pic24">
+          <NavLink to="/locations"
+          className='pic2'
+          style={({ isActive }) => ({
+            color: isActive ? '#ffffff' : '#ffffff',
+            textDecoration: isActive ? 'none' : 'none',
+          })}>
+              <p>Munich</p>
+          </NavLink>
+          <NavLink to="/locations" className='pic4'
+          style={({ isActive }) => ({
+            color: isActive ? '#ffffff' : '#ffffff',
+            textDecoration: isActive ? 'none' : 'none',
+          })}>
+              <p>Frankfurt</p>
+          </NavLink>
+          </div>
+          <NavLink to="/locations" className='pic3'
+          style={({ isActive }) => ({
+            color: isActive ? '#ffffff' : '#ffffff',
+            textDecoration: isActive ? 'none' : 'none',
+          })}>
+              <p>Hannover</p>
+          </NavLink>
+          <NavLink to="/locations" className='pic5'
+          style={({ isActive }) => ({
+            color: isActive ? '#ffffff' : '#ffffff',
+            textDecoration: isActive ? 'none' : 'none',
+          })}>
+              <p>Berlin</p>
+          </NavLink>
+          <NavLink to="/locations" className='pic6'
+          style={({ isActive }) => ({
+            color: isActive ? '#ffffff' : '#ffffff',
+            textDecoration: isActive ? 'none' : 'none',
+          })}>
+              <p>Leipzig</p>
+          </NavLink>
+          </div>
+        </div>
+      </div>
     </div>   
    
   );
