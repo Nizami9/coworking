2946ebe9873be8bdc76f58b8e2f33a054c3e487a
Nizami Suleymanov <nizami.suleymanoff@gmail.com>
2023-01-05 12:54:06 +0400

    changed margin-top of 3 lines

--- src/Components/Navbar/NavbarElements.js
+++ src/Components/Navbar/NavbarElements.js
@@ -66,7 +66,7 @@ export const Bars = styled(FaBars)`
   @media screen and (max-width: 768px) {
     display: block;
     position: absolute;
-    top: 0;
+    top: -5px;
     right: 0;
     transform: translate(-100%, 75%);
     font-size: 1.8rem;
