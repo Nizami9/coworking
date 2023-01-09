cf9c38a6e50a01c8559030dea4fe7d74380e5367
Nizami Suleymanov <nizami.suleymanoff@gmail.com>
2023-01-09 14:45:08 +0100

    first step in locations id

--- src/Components/MyProfile/Space/Space.js
+++ src/Components/MyProfile/Space/Space.js
@@ -1,29 +1,31 @@
 import React from 'react';
 import Navbar from "../../Navbar/index";
 import Footer from "../../Footer/Footer";
-import Image1 from "./ImagesSpace/Image1.png";
-import Image2 from "./ImagesSpace/Image2.png";
-import Image3 from "./ImagesSpace/Image3.png";
 import calendar from "./ImagesSpace/calendar.png";
 import schedule from "./ImagesSpace/schedule.png";
+import SpaceData from "../../../spaceData.json";
 import "./Space.css";
 
 const Space = () => {
+    return (
+        <div>
+    {SpaceData &&
+        SpaceData.map((space) => {
   return (
     <div className='containerSpace'>
         <div className='spaceImages'>
             <div className='leftImageSpace'>
-                <img src={Image1}></img>
+                <img src={space.imgUrl}></img>
             </div>
-            <div className='rightImageSpace'>
-                <img src={Image2}></img>
-                <img src={Image3}></img>
-            </div>
+            {/* <div className='rightImageSpace'>
+                <img src={space.imgUrl}></img>
+                <img src={space.imgUrl}></img>
+            </div> */}
         </div>
         <div className='spaceReservations'>
-                <h1>Keyboards & Dreams Royal </h1>
+                <h1>{space.title}</h1>
                 <span className='info'>Available 57 desks</span>
-            <p className='spacePrice'>25$ / day</p>
+            <p className='spacePrice'>{space.costperDay} / day</p>
             <p className='reserveOnlineP'>Reserve online</p>
             <button className='reserveSpaceButton'><p>Reserve space</p></button>
             <div className='hrOr'>
@@ -51,14 +53,18 @@ const Space = () => {
                 <h3>Description:</h3>
                 <div className='descriptionText'>
                     <p>
-                    WIRED magazine included our central London building in their ten "UK's best co-working spaces (that aren't WeWork)". And now we're at it again, brand new for 2021 - bringing the coolest, cosiest workplace to the Royal Docks, on a pedestrianised street alongside a coffee shop, a nursery, an ice cream factory and a gym.
-                    5 minutes walk from "Royal Victoria" and "Canning Town" stations - similarly to our flagship space in Farringdon, we're providing the essentials at great affordable startup & freelancer friendly rates. We're in a new ultra-sustainable building, powered by 100% renewable energy with a focus on minimising our impact on the world.
+                    {space.description}
                     </p>
                 </div>
             </div>
 </div>
         </div>
-  )
+                )
+            }
+        )
+    }
+    </div>
+    )
 }
 
-export default Space
+export default Space;
--- src/Components/ReserveComponents/List.js
+++ src/Components/ReserveComponents/List.js
@@ -1,5 +1,6 @@
 import Space from "../../spaceData.json";
-
+import { Link } from "react-router-dom";
+import "./link.css";
 const List = () => {
   return (
     <div className="locations">
@@ -17,6 +18,7 @@ const List = () => {
                     <div className="address"> {space.address}</div>
                     <div className="city"> {space.city} </div>
                     <div className="country"> {space.counrty} </div>
+                    <div><Link className="spacesLink" to='/space/:id'>Explore space →</Link></div>
                     <div className="area-maxPeople-costperDay">
                     <div className="area"> {space.area}</div>
                     <div className="maxpeople"> {space.maxPeople} </div>
