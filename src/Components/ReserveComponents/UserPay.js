97dc593d1543f36f6ff98e3f6631ad403d58f866
kingsley <rawmetal77@gmail.com>
2023-01-12 18:02:19 +0100

    updated visa card payment

--- src/Components/ReserveComponents/UserPay.js
+++ src/Components/ReserveComponents/UserPay.js
@@ -2,15 +2,17 @@ import React from "react";
 import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
-import { useBookContext } from '../../context/BookContext';
 
+import { FieldSet, InputField } from "fannypack";
+import { usePaymentInputs } from "react-payment-inputs";
 
 const UserPay = () => {
+  const { meta, getCardNumberProps, getExpiryDateProps, getCVCProps } =
+    usePaymentInputs();
+  const { erroredInputs, touchedInputs } = meta;
+
   const { register, handleSubmit } = useForm();
   const [data, setData] = useState("");
-
-  const {formDetails}= useBookContext();
-  console.log("form details from context ",formDetails )
 
   return (
     <div className="paymentSelection-payM">
@@ -24,14 +26,13 @@ const UserPay = () => {
         </Link>
       </div>
       <div className="payM-wrapper">
-        {/* <div className="line">line</div> */}
         <div className="inputForm">
           <div className="payM">
             {" "}
             <h1>Payment selection</h1>
           </div>
           <div className="input-wrape">
-            <form 
+            <form
               onSubmit={handleSubmit((data) => setData(JSON.stringify(data)))}
             >
               <h6>Payment method</h6>
@@ -42,7 +43,7 @@ const UserPay = () => {
                   width: "450px",
                 }}
                 {...register("payment method")}
-                placeholder="Apply pay"
+                placeholder="Visa"
               />
               <br></br>
               <h6>Name</h6>
@@ -54,22 +55,70 @@ const UserPay = () => {
                   width: "450px",
                 }}
                 {...register("name")}
-                placeholder="kingsley best"
+                placeholder="name    surname"
               />
-              <br></br>
-              <h6>Card Number</h6>
-              <input
-                required
+
+              <div className="visa-card">
+                <FieldSet>
+                  <h6>Card number</h6>
+                  <InputField
+                    // Here is where React Payment Inputs injects itself into the input element.
+                    {...getCardNumberProps()}
+                    placeholder="xxxx xxxx xxxx xxxx"
+                    // label="Card number"
+                    inputRef={getCardNumberProps().ref}
+                    // You can retrieve error state by making use of the error & touched attributes in `meta`.
+                    state={
+                      erroredInputs.cardNumber && touchedInputs.cardNumber
+                        ? "danger"
+                        : undefined
+                    }
+                    validationText={
+                      touchedInputs.cardNumber && erroredInputs.cardNumber
+                    }
+                    maxWidth="750px" background="#FFFFFF"
+                  />
+
+        
+
+                  <div className="expiry-cvc">
+                   
+                    <InputField
+                      {...getExpiryDateProps()}
+                      label="Expiry"
+                      inputRef={getExpiryDateProps().ref}
+                      state={
+                        erroredInputs.expiryDate && touchedInputs.expiryDate
+                          ? "danger"
+                          : undefined
+                      }
+                      validationText={
+                        touchedInputs.expiryDate && erroredInputs.expiryDate
+                      }
+                      maxWidth="8rem" 
+                    />
+
+                    <div className="cvv">
+                      <InputField
+                        {...getCVCProps()}
+                        placeholder="123"
+                        label="CVC"
+                        inputRef={getCVCProps().ref}
+                        state={
+                          erroredInputs.cvc && touchedInputs.cvc
+                            ? "danger"
+                            : undefined
+                        }
+                        validationText={touchedInputs.cvc && erroredInputs.cvc}
+                        maxWidth="rem" 
+                      />
+                    </div>
+                  </div>
+                </FieldSet>
+              </div>
+
+              <button
                 className="form-2"
-                style={{
-                  height: "44px",
-                  width: "450px",
-                }}
-                {...register("Card Number")}
-                placeholder="xxxx xxxx xxxx xxxx"
-              ></input>
-              <br></br>
-              <button className="form-2"
                 style={{
                   height: "44px",
                   width: "200px",
@@ -77,7 +126,7 @@ const UserPay = () => {
                 }}
                 type="submit"
               >
-                <Link to='/thank-you'>Complete</Link>
+                <Link to="/thank-you">Complete</Link>
               </button>
             </form>
           </div>
